c61ee2da5d2296472cc85f51351dda17b8b4689d
Jason Tran <etrananhphong@gmail.com>
2020-06-02 18:04:00 -0400

    IOS-32: I Am Poor App

---
+++ section_2/section_3/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jason Tran on 2020-06-02.
+//  Copyright © 2020 Jason Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
